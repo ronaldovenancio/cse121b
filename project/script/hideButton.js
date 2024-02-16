c46ea230c2f6163deb0e68278c876cd12206be4b
Ronaldo Venâncio da Silva <95707584+ronaldovenanciorvs@users.noreply.github.com>
2024-02-16 16:03:33 -0300

    upoald files to project

---
+++ project/script/hideButton.js
@@ -0,0 +1,13 @@
+function hideButton() {
+    let element = document.getElementById("startGame");
+    if (
+      document.getElementById("correctScore").innerText == "0" &&
+      document.getElementById("incorrectScore").innerText == "0"
+    ) {
+      element.style.visibility = "hidden";
+    } else {
+      element.style.visibility = "visible";
+    }
+  }
+
+  export default hideButton;
